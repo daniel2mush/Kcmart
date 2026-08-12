e2bc71f6b0296b603503cc3567b771a05035d7ed
Daniel <daniel2mush@gmail.com>
2026-08-12 19:15:34 +0000

    made changes to the config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,25 +3,17 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start' // correct package
 
 export default defineConfig({
   ssr: {
     noExternal: ['gsap', 'GSAP', '@gsap/react'],
   },
-
-  resolve: {
-    tsconfigPaths: true,
-  },
-
   plugins: [
     devtools(),
     tanstackStart(),
-    netlify(),
+    netlify(), // now this works
     tailwindcss(),
     viteReact(),
   ],
 })
-function netlify(): import("vite").PluginOption {
-    throw new Error('Function not implemented.')
-}
-
